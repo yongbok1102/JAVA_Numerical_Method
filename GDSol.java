bfeb9e7209ffd4385ee42b3edc6612d72e14cac0
yongbok1102 <yongbok1102@gmail.com>
2016-12-30 23:33:02 +0900

    Update GDSol.java

--- GDSol.java
+++ GDSol.java
@@ -2,6 +2,8 @@ import java.io.*;
 
 // A Java code for solving linear system using Gradient Descent Method
 //x_{n+1} = x_{n} - stp*AT(Ax-b)
+//stp is determined by
+// stp = (x_{n}-x_{n-1})(grad(F)_{n}-grad(F)_{n-1})/(norm(F_{n}-F_{n-1}))^2
 
 class GDSol{
     public static void main(String[] args) throws IOException{
@@ -13,6 +15,12 @@ class GDSol{
         //unknowns
         double[] x; x = new double[num];
         
+        //old values
+        double[] xold; xold = new double[num];
+        
+        //old-old values
+        double[] xoold; xoold = new double[num];
+        
         //initiating x
         for(int i=0; i<num; i++)
         {
@@ -20,9 +28,9 @@ class GDSol{
             String str = br.readLine();
             x[i] = Double.parseDouble(str);
         }
-        System.out.println("Enter the step parameter");
-        String str2 = br.readLine();
-        double stp = Double.parseDouble(str2);   
+        
+        double stp;
+        
         System.out.println("Enter the maximum iteration number");
         String str3 = br.readLine();
         int itrmax = Integer.parseInt(str3);          
@@ -30,7 +38,13 @@ class GDSol{
         double[][] A; A = new double[num][num];
         double[] b; b = new double[num];
         double[] sumAx; sumAx = new double[num];
+        double[] sumAxold; sumAxold = new double[num];
+        double[] sumAxoold; sumAxoold = new double[num];        
         double[] grad; grad = new double[num];
+        double[] gradold; gradold = new double[num];
+        double[] gradoold; gradoold = new double[num];
+        double[] difx; difx = new double[num];
+        double[] difgrad; difgrad = new double[num];
         int itr = 0;
         for(int i=0; i<num; i++)
         {
@@ -48,7 +62,43 @@ class GDSol{
                 b[i] = Double.parseDouble(str);            
         }
         
+        System.out.println("Enter the initial step parameter.");
+        String str4 = br.readLine();
+        stp = Double.parseDouble(str4);   
         double tol = 1e-013;
+        
+        for(int i=0;i<num;i++)
+        {
+            sumAx[i] = 0;
+            grad[i] = 0;
+        }
+        for(int i=0;i<num;i++)
+        {
+            xold[i] = x[i];
+        }
+        
+        for(int i=0; i<num;i++)
+        {
+            for(int j=0;j<num;j++)
+            {
+                sumAx[i] += A[i][j]*x[j];
+            }
+        }
+        for(int i=0;i<num;i++)
+        {
+            for(int j=0;j<num;j++)
+            {
+                grad[i] +=A[j][i]*(sumAx[i] - b[i]);
+            }
+        }
+        for(int i=0;i<num;i++)
+        {
+            x[i] = x[i] - stp*grad[i];
+        }
+        for(int i=0;i<num;i++)
+        {
+            sumAx[i] = 0;
+        }
         for(int i=0; i<num;i++)
         {
             for(int j=0;j<num;j++)
@@ -57,46 +107,84 @@ class GDSol{
             }
         }
         double res = calRes(sumAx,b,num);
-        while(res>tol)
-        {
+        itr = itr + 1;
+        
+        while(res>tol){
             itr++;
             for(int i=0; i<num; i++)
             {
-                sumAx[i] = 0;
+                xoold[i] = xold[i];
             }
+            
             for(int i=0; i<num; i++)
             {
+                xold[i] = x[i];
+            }
+            
+            for(int i=0; i<num; i++){
+                sumAx[i] = 0;
+                sumAxold[i] = 0;
+                sumAxoold[i] = 0;
                 grad[i] = 0;
-            }            
-            for(int i=0; i<num; i++)
+                gradold[i] = 0;
+                gradoold[i] = 0;
+            }
+            
+            for(int i=0;i<num;i++)
             {
-            
-                for(int j=0; j<num; j++)
-                {
-                    sumAx[i] += A[i][j]*x[j];
-            
-                }
-                
-                //calculating gradient
                 for(int j=0;j<num;j++)
                 {
-                    grad[i]+=A[j][i]*(sumAx[i]-b[i]);
+                    sumAxold[i] += A[i][j]*xold[j];
+                    sumAxoold[i] += A[i][j]*xoold[j];                    
                 }
-                //solving linear system
-                x[i] = x[i] - stp*grad[i];              
             }
             
-            //calculating residuals (sum(|Ax-b|)/sum(|b|))
+            for(int i=0; i<num; i++)
+            {
+                for(int j=0; j<num; j++)
+                {
+                    gradold[i] += A[j][i]*(sumAxold[j] - b[j]);
+                    gradoold[i] += A[j][i]*(sumAxoold[j] - b[j]);
+                }
+            }
+            
+            for(int i=0; i<num; i++)
+            {
+                difgrad[i] = gradold[i] - gradoold[i];
+                difx[i] = xold[i] - xoold[i];
+            }
+            
+            stp = innerProd(difx, difgrad,num)
+            * Math.pow(Norm(difgrad,num),-2);
+            
+            for(int i=0; i<num; i++)
+            {
+                x[i] = xold[i] - stp*gradold[i];
+            }
+            
+            for(int i=0;i<num;i++)
+            {
+                sumAx[i] = 0;
+            }
+            for(int i=0; i<num;i++)
+            {
+                for(int j=0;j<num;j++)
+                {
+                    sumAx[i] += A[i][j]*x[j];
+                }
+            }
+            
             res = calRes(sumAx,b,num);
+            
             if(itr==itrmax)
                 break;
         }
-        System.out.print("iter: "+ itr);
-        for(int i=0; i<num; i++)
-        {
-                System.out.print(", x_" + (i+1) + '=' + x[i]);
+        System.out.print("itr: " + itr);
+        for(int i=0; i<num; i++){
+            System.out.print("x_"+(i+1)+" = " + x[i] + ", ");
         }
-        System.out.print(", res: " + res);
+        System.out.println("resid: " + res);
+        
     }
     
     public static double calRes(double Ax[], double b[], int n)
@@ -112,5 +200,25 @@ class GDSol{
             sumB +=b[i]*b[i];
         }
         return Math.sqrt(res/sumB);
+    }
+    
+    public static double Norm(double A[], int n)
+    {
+        double sum = 0;
+        for(int i=0; i<n; i++)
+        {
+            sum += A[i]*A[i];
+        }
+        return Math.sqrt(sum);
+    }
+    
+    public static double innerProd(double x[], double y[], int n)
+    {
+        double sum = 0;
+        for(int i=0; i<n; i++)
+        {
+            sum += x[i]*y[i];
+        }
+        return sum;
     }
 }
